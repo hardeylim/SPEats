200ce7a3478a4b997656b07bea0e3eab0aecc9e3
Hardey <hardeylim@gmail.com>
2020-04-07 23:34:09 +0800

    Fix associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,6 @@ class Restaurant < ApplicationRecord
 
   has_many :restaurant_photos
   has_many :menu_templates
+
+  belongs_to :default_menu_template, class_name: 'MenuTemplate', foreign_key: :default_menu_template_id
 end
